96bde549c5f0a7ca63ad69fc4cb8de6f46daab39
JackLiR8 <jiakeli1024@gmail.com>
2019-12-13 10:25:14 +0800

    :recycle: Refactor: 棋盘采用双循环渲染

--- src/demo/ticTacToe.js
+++ src/demo/ticTacToe.js
@@ -22,31 +22,31 @@ function Square(props) {
 class Board extends React.Component {
   renderSquare(i) {
     return (
-      <Square 
+      <Square
+        key={i} 
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)} 
       />
     );
   }
 
+  borderTd(start) {
+    return [0, 1, 2].map(item => {
+      return this.renderSquare(item + start)
+    })
+  }
+
   render() {
+    const rows = [0, 3, 6].map((start, i) => {
+      return (
+        <div className="board-row" key={i}>
+          {this.borderTd(start)}
+        </div>
+      )
+    })
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
