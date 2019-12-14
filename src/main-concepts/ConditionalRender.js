7172246cfdd43ef6a97d549ba1b25000893098a6
JackLiR8 <jiakeli1024@gmail.com>
2019-12-14 11:40:04 +0800

    :sparkles: New Featrue: 核心概念-条件渲染完成

--- src/main-concepts/ConditionalRender.js
+++ src/main-concepts/ConditionalRender.js
@@ -2,6 +2,7 @@
  * @file 条件渲染
  */
 import React from 'react'
+import './ConditionalRender.css'
 
 function UserGreeting() {
   return <h3>Welcome back!</h3>
@@ -59,7 +60,7 @@ class LoginControl extends React.Component {
     let button;
 
     if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleClick} / >
+      button = <LogoutButton onClick={this.handleClick} />
     } else {
       button = <LoginButton onClick={this.handleClick} />
     }
@@ -71,14 +72,72 @@ class LoginControl extends React.Component {
       </div>
     )
   }
+}
 
+/**
+ * 条件渲染中&&运算符的应用
+ */
+function Mailbox(props) {
+  const { unreadMessages } = props;
+  return (
+    <div>
+      <h3>Hello</h3>
+      {unreadMessages.length > 0 &&
+        <p>
+          You have <strong>{unreadMessages.length}</strong> unread messages.
+        </p>
+      }
+    </div>
+  )
+}
+
+/**
+ * 阻止组件渲染(render方法返回null)
+ */
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning
+    </div>
+  )
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showWarning: true };
+  }
+
+  handleToggleClick = () => {
+    this.setState(state => ({
+      showWarning: !state.showWarning,
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    )
+  }
 }
 
 function ConRender() {
+  const messages = ['ha', 'foo', 'bar'];
   return (
     <div>
       <h2>Conditional Rendering</h2>
       <LoginControl />
+      <Mailbox unreadMessages={messages} />
+      <Page />
     </div>
   )
 }
