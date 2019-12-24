b7f9d96123be51cda6b904af283a725bdd038d20
JackLiR8 <jiakeli1024@gmail.com>
2019-12-24 23:18:47 +0800

    :sparkles: new feature: hooks: useReducer 初始化state方法，应用场景

--- src/hook/useReducer.js
+++ src/hook/useReducer.js
@@ -1,8 +1,22 @@
 /**
  * @file useReducer
+ * 
+ * 应用场景
+ * 1. state逻辑复杂且包含多个值
+ * 2. state依赖之前的state
+ * 3. 给触发深更新的组件做性能优化
+ * 
+ * 指定初始state
+ * 1. 将初始值作为第二个参数
+ * 2. 惰性初始化
+ *    将init函数作为useReducer的第三个参数
  */
 
 import React, { useReducer } from 'react'
+
+function init(initialCount) {
+  return {count: initialCount}
+}
 
 function reducer(state, action) {
   switch (action.type) {
@@ -10,20 +24,23 @@ function reducer(state, action) {
       return {count: state.count + 1}
     case 'decrement':
       return {count: state.count - 1}
+    case 'reset':
+      return init(action.payload)
     default:
       throw new Error()
   }
 }
 
-const initialState = { count: 0 }
-
-function Counter() {
-  const [state, dispatch] = useReducer(reducer, initialState)
+function Counter({initialCount}) {
+  const [state, dispatch] = useReducer(reducer, initialCount, init)
 
   return (
     <div>
       Count: {state.count}
       <br/>
+      <button onClick={() => dispatch({type: 'reset', payload: initialCount})}>
+        reset
+      </button>
       <button onClick={() => dispatch({type: 'decrement'})}>
         -
       </button>
@@ -38,7 +55,7 @@ function ReducerBox() {
   return (
     <fieldset>
       <legend><h2>useReducer</h2></legend>
-      <Counter />
+      <Counter initialCount={4} />
     </fieldset>
   )
 }
