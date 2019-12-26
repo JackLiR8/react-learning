a1aeb139fc486c37b60c93e7a84dd8544f2a6817
JackLiR8 <jiakeli1024@gmail.com>
2019-12-26 22:46:42 +0800

    :boom: 引入路由

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import Main from './main-concepts/ApiDisplay';
 import Advanced from './advanced-guides/AdvancedDemo';
 import HookDisplay from './hook/HookDisplay'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
-function App() {
+/* function App() {
   return (
     <div className="App">
       <Game /><hr/>
@@ -14,6 +20,36 @@ function App() {
       <HookDisplay />
     </div>
   );
+} */
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <fieldset>
+            <legend><h2>Nav</h2></legend>
+            <Link className="link" to="/" >Home</Link>&nbsp;
+            <Link className="link" to="/advanced" >Advanced</Link>&nbsp;
+            <Link className="link" to="/hooks">Hooks</Link>&nbsp;
+          </fieldset>
+        </nav>
+
+        <Switch>
+          <Route path="/advanced">
+            <Advanced />
+          </Route>
+          <Route path="/hooks">
+            <HookDisplay />
+          </Route>
+          <Route path="/">
+            <Game />
+            <Main />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
