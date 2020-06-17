53f32affae9a500a6b39fc0490bff08028adb7cd
JackLiR8 <jiakeli1024@gmail.com>
2020-06-17 11:01:32 +0800

    :construction: antd-form 进行中

--- src/antd/Form.jsx
+++ src/antd/Form.jsx
@@ -4,27 +4,119 @@ import { Form, Input, Button } from 'antd'
 const FormItem = Form.Item
 
 class TryForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      form: {
-        userName: ''
+  state = {
+    confirmDirty: false,
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
       }
+    });
+  }
+
+  /**
+   * 验证密码
+   */
+  validateToNextPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
+  }
+
+  /**
+   * 验证确认密码 
+   */
+  compareToFirstPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Two passwords that you enter is inconsistent!');
+    } else {
+      callback();
     }
   }
+
+  handleConfirmBlur = e => {
+    const { value } = e.target;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
   
   render() {
+    const { getFieldDecorator } = this.props.form
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 16,
+          offset: 8,
+        },
+      },
+    };
+
     return (
       <div>
-        <Form>
-          <FormItem
-            name="userName"
-            label="姓名"
-            rules={[{ required: true, message: '请输入用户名' }]}>
-            <Input />
+        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+          <FormItem label="姓名" >
+            { getFieldDecorator('userName', {
+              rules: [
+                {
+                  required: true,
+                  message: 'userName is required'
+                }
+              ]
+            })(<Input />)}
           </FormItem>
-          <FormItem>
-            <Button type="primary" htmlType="submit">提交</Button>
+
+          <FormItem label="密码" hasFeedback>
+            {getFieldDecorator('password', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+                {
+                  validator: this.validateToNextPassword,
+                }
+              ]
+            })(<Input.Password />)}
+          </FormItem>
+
+          <FormItem label="确认密码" hasFeedback>
+            {getFieldDecorator('confirm', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
+                },
+                {
+                  validator: this.compareToFirstPassword,
+                },
+              ]
+            })(<Input.Password onBlur={this.handleConfirmBlur} />)}
+          </FormItem>
+
+          <FormItem {...tailFormItemLayout}>
+            <Button 
+              type="primary" 
+              htmlType="submit">提交</Button>
           </FormItem>
         </Form>
       </div>
@@ -32,4 +124,4 @@ class TryForm extends React.Component {
   }
 }
 
-export default TryForm
+export default Form.create({})(TryForm)
